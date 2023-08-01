199b20d00c533e8f38cad7eaf0d9c4315bd6d9b7
Agustin Ignacio Guarino Bonahon <agustinignacio.guarinobonahon@ukg.com>
2023-08-01 14:27:20 -0300

    Basic class structure

--- easyemail.py
+++ easyemail.py
@@ -1,6 +1,14 @@
 import keyboard
 
-while True:
-    if keyboard.is_pressed('ctrl+1'):
-        print("Test")
-        break
+class EasyEmail:
+    def __init__(self):
+        pass
+
+    def startListener(self):
+        while True:
+            if keyboard.is_pressed('ctrl+1'):
+                print("Test")
+                break
+
+    def copyToClipboard(self):
+        pass
